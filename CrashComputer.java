cf095cd76cca18dfcee16a976c5687f6de6aaaea
Piyush Acharya <acharyapiyush1@outlook.com>
2022-09-29 22:19:12 -0700

    Added file

---
+++ CrashComputer.java
@@ -0,0 +1,21 @@
+package DNAnalyzer;
+
+public class CrashComputer {
+    public static void crashComputer() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        crashComputer();
+                    }
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        crashComputer();
+    }
+}
